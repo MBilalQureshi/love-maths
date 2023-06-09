a3500538fdf090add9e85e2f0ee5d38b0bd1f9a0
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-06-09 15:07:18 +0000

    Add code to calculate two random numbers for the game questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,13 @@ document.addEventListener("DOMContentLoaded",function(){
     }
 })
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user answer has been processed
+ */
 function runGame() {
-
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num12 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
